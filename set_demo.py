b86c5f068829d2f4fb80f6ff5cfa9eaea252bd77
DaBigM <mahdi.shokri@gmail.com>
2022-05-03 22:43:51 +0430

    Second Commit for 'set' Data Type

--- set_demo.py
+++ set_demo.py
@@ -1,4 +1,5 @@
 # unordered data type
+# mutable
 
 # you 'CANNOT' define empty set using literals
 emptySet = set()
@@ -7,7 +8,6 @@ sampleSet = {1, 'A', 'g', 3.86, 'Dayche', 'A', 'A'}
 for _ in range(10):
     print(sampleSet)
 
-print('=' * 40)
 
 odd = set(range(1, 50, 2))  # members: 25
 power = {1, 4, 9, 16, 25, 36, 49}  # members: 7
@@ -15,5 +15,19 @@ power = {1, 4, 9, 16, 25, 36, 49}  # members: 7
 print(len(odd))
 print(odd.intersection(power))
 print(len(odd.union(power)))
+
+print('=' * 40)
+print(odd.intersection_update(power))
+print(odd)
+
+power_frozen = frozenset(power)
+
+
+
+
+
+
+
+
 
 
