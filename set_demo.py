e6a09dc04b3fd9366e7f5e3ffcef0c198063872c
DaBigM <mahdi.shokri@gmail.com>
2022-05-03 23:12:06 +0430

    Third Commit for 'set' Data Type - Added Extra Print

--- set_demo.py
+++ set_demo.py
@@ -8,6 +8,7 @@ sampleSet = {1, 'A', 'g', 3.86, 'Dayche', 'A', 'A'}
 for _ in range(10):
     print(sampleSet)
 
+print('+' * 40)
 
 odd = set(range(1, 50, 2))  # members: 25
 power = {1, 4, 9, 16, 25, 36, 49}  # members: 7
