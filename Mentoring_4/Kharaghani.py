ddaea208b05c19a842673a11bacb801d09ef632c
DaBigM <mahdi.shokri@gmail.com>
2022-05-22 20:47:51 +0430

    Mentoring Session 4

---
+++ Mentoring_4/Kharaghani.py
@@ -0,0 +1,9 @@
+with open('sample.txt', 'w') as fh:
+    print('Mentoring Session 4', file=fh)
+    print('Ms Kharaghani in soal ro porsidand', file=fh)
+    print('Emrooz 22 May 2022 saat 20:07', file=fh)
+
+with open('sample.txt', 'a') as fh:
+    print('=' * 40, file=fh)
+    print('in be akharesh ezafeh mishe', file=fh)
+
---
+++ Mentoring_4/Masoudi.py
@@ -0,0 +1,12 @@
+# Annotation
+
+def annoTest(sep: str = ' ', fact: int = 40) -> str:
+    print('this is a test', 'Dayche DS Class', sep=sep)
+    print('=' * fact)
+    return 'bah bah'
+
+
+out = annoTest(sep='\t')
+
+
+
