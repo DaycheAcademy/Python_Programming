39c34533db360a29aeefca2ec6d8fe991b804741
Zahra Kharaghani <95737269+zkharaghani@users.noreply.github.com>
2022-06-16 16:10:44 +0430

    Update calc_mean_value.py

--- calc_mean_value.py
+++ calc_mean_value.py
@@ -1,25 +1,22 @@
 # force a user to input an integer
 # calculate mean of unknown number of integers (q or quit to stop entering data)
 
-counter = 0
 sumOfValues = 0
-
+counter = 0
 while True:
-    inp = input('Enter an Integer: ')
-    # print(type(inp))
-
+    inp = input("Enter an integer: ")
     if inp.lower() == 'q' or inp.lower() == 'quit':
         break
     if not inp.isdigit():
         continue
     else:
-        sumOfValues = sumOfValues + int(inp)
-        counter = counter + 1
-
-meanValue = sumOfValues / counter
+        sumOfValues += float(inp)
+        counter += 1
+if counter:
+    meanValue = sumOfValues / counter
+    print(f'The mean of numbers is equal to {meanValue:.2f}')
+else:
+    meanValue = None
 print(meanValue)
-
-
-# print(inp.isdigit())
 
 
