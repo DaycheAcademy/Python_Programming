39173cc03edb4299c446082176cf6fecb458d547
Mehdi Shokri Zadeh <mahdi.shokri@gmail.com>
2023-04-18 21:15:47 +0430

    Added Solution for CSV Question

---
+++ Week 4/csv_solution.py
@@ -0,0 +1,18 @@
+
+email = list()
+year = list()
+name = list()
+surname = list()
+
+
+with open('sample.csv') as csv_file:
+    line = csv_file.readline().split(';')
+    while line:
+        email.append(line[0])
+        year.append(line[1])
+        name.append(line[2])
+        surname.append(line[3])
+        line = csv_file.readline().split(';')
+
+
+
